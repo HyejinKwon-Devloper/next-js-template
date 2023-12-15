892c23fdcefc74c39db0accc749356c065a43ecf
DESKTOP-JR4PBRF\JIN <hanj8980@gmail.com>
2023-12-15 16:40:54 +0900

    feature(#2): darkmode 환경 설정 - next-themes 패키지 설치 및 환경 설정 - 다크모드 style 반영

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,8 +1,17 @@
+'use client';
+import { useTheme } from 'next-themes';
 import Image from 'next/image';
+
 export default function AppBar() {
+  const { theme, setTheme } = useTheme();
+
+  function handleDarkMode() {
+    return setTheme(theme === 'light' ? 'dark' : 'light');
+  }
+
   return (
     <div className="appbar">
-      <div className="appbar-profile">
+      <div className="appbar-profile appbar-ico">
         <Image
           src="/ico/account_circle_black.svg"
           alt="account profile"
@@ -21,25 +30,27 @@ export default function AppBar() {
         />
       </div>
       <div className="appbar-title">
-        <h1>appbar title</h1>
+        <h1>Hello Jin's World ❤</h1>
       </div>
       <div className="appbar-ico">
-        <Image
-          src="/ico/light_mode_black.svg"
-          alt="light mode"
-          className="imgLight"
-          width={20}
-          height={20}
-          priority
-        />
-        <Image
-          src="/ico/dark_mode_black.svg"
-          alt="dark mode"
-          className="dark:dark-invert imgDark"
-          width={20}
-          height={20}
-          priority
-        />
+        <button onClick={handleDarkMode}>
+          <Image
+            src="/ico/light_mode_black.svg"
+            alt="light mode"
+            className="imgLight hover:opacity-25 hover:animate-spin"
+            width={20}
+            height={20}
+            priority
+          />
+          <Image
+            src="/ico/dark_mode_black.svg"
+            alt="dark mode"
+            className="dark:dark-invert imgDark hover:opacity-25 hover:animate-spin"
+            width={20}
+            height={20}
+            priority
+          />
+        </button>
       </div>
       <div className="appbar-ico">
         <Image
