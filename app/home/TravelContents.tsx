d79e47e46de07e6343c25550639df51039ae9e12
DESKTOP-JR4PBRF\JIN <hanj8980@gmail.com>
2025-04-30 13:31:45 +0900

    feture(#39) : caraousel design

--- app/home/TravelContents.tsx
+++ app/home/TravelContents.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image';
 import Card from '@/components/card/Card';
 import Button from '@/components/Button';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const dataSet = [
   {
@@ -89,58 +89,138 @@ const dataSet = [
     startDate: '2023.04.30',
     endDate: '2023.05.05',
   },
+  {
+    src: '/img/20230503_141710.jpg',
+    alt: 'titleImg',
+    title: 'Sapppro13',
+    startDate: '2023.04.30',
+    endDate: '2023.05.05',
+  },
+  {
+    src: '/img/20230503_141710.jpg',
+    alt: 'titleImg',
+    title: 'Sapppro14',
+    startDate: '2023.04.30',
+    endDate: '2023.05.05',
+  },
+  {
+    src: '/img/20230503_141710.jpg',
+    alt: 'titleImg',
+    title: 'Sapppro15',
+    startDate: '2023.04.30',
+    endDate: '2023.05.05',
+  },
 ];
 export default function Contents() {
-  const [currentIndex, setIndex] = useState<number>(1);
-  
-  function handlePrevButton(){
-    setIndex((prev) => prev-1);
+  const cardSize = 210;
+  const [currentIndex, setIndex] = useState<number>(0);
+  const [containerWidth, setWidth] = useState<number>(0);
+  const [moveX, setX] = useState<number>(0);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const limit = Math.floor((window.screen.width - 309 - 16 - 76) / cardSize);
+
+  function handlePrevButton() {
+    setIndex((prev) => (prev - 1 < 0 ? 0 : prev - 1));
   }
-  function handleNextButton(){
-    setIndex((prev) => prev+1);
+  function handleNextButton() {
+    setIndex((prev) => (prev + 1) % (dataSet.length - limit + 1));
   }
+
+  useEffect(() => {
+    if (currentIndex === 0) {
+      setX(0);
+      return;
+    }
+    if (currentIndex === dataSet.length - limit) {
+      setX(dataSet.length * cardSize - containerWidth + 30 + 30);
+    } else {
+      setX((prev) => prev + cardSize);
+    }
+  }, [currentIndex]);
+
+  useEffect(() => {
+    if (divRef.current) {
+      const width = divRef.current.offsetWidth;
+      setWidth(width);
+    }
+  }, [divRef]);
+
   return (
-    <div className="sub-container">
-    <div className='carousel-container'>
-      <Button className='btn-prev' onClick={handlePrevButton}>
-        <Image src="/ico/add_black.svg" alt="prev" width={100} height={100}/>
-      </Button>
-      {dataSet?.map((data) => {
-        return (
-          <div style={{
-            transform: `translateX(-${(currentIndex * (300 + 20))}px)`,
-            transition: "transform 0.5s ease-in-out"
-          }}>
-          <Card>
-            <div className="card-title">
-              <Image src={data.src} alt={data.alt} width={260} height={232} />
+    <div className="sub-container" ref={divRef}>
+      {dataSet.length > limit && (
+        <Button className="btn-prev" onClick={handlePrevButton}>
+          <Image
+            src="/ico/chevron_left_black.svg"
+            alt="prev"
+            className="imgLight"
+            width={30}
+            height={30}
+          />
+          <Image
+            src="/ico/chevron_left_white.svg"
+            alt="prev"
+            className="imgDark"
+            width={30}
+            height={30}
+          />
+        </Button>
+      )}
+      <div className="carousel-container">
+        {dataSet?.map((data) => {
+          return (
+            <div
+              style={{
+                transform: `translateX(-${moveX}px)`,
+                transition: 'transform 0.5s ease-in-out',
+              }}
+              key={data.title}
+            >
+              <Card>
+                <div className="card-title">
+                  <Image
+                    src={data.src}
+                    alt={data.alt}
+                    width={260}
+                    height={232}
+                  />
+                </div>
+                <div>
+                  <h2>{data.title}</h2>
+                  <span>{data.startDate}</span>
+                  <em>~</em>
+                  <span>{data.endDate}</span>
+                </div>
+                <div className="action">
+                  <div>
+                    <Button buttontype="filled" isfull>
+                      내 여행 보러 가기
+                    </Button>
+                  </div>
+                </div>
+              </Card>
             </div>
-            <div>
-              <h2>{data.title}</h2>
-              <span>{data.startDate}</span>
-              <em>~</em>
-              <span>{data.endDate}</span>
-            </div>
-            <div className="action">
-              <div>
-                <Button buttontype="filled" isfull>
-                  자세히 보기
-                </Button>
-              </div>
-              <div>
-                <Button buttontype="outlined" isfull>
-                  뒤로가기
-                </Button>
-              </div>
-            </div>
-          </Card>
-          </div>
-        );
-      })}
-      <Button className='btn-next' onClick={handleNextButton}>
-        <Image src="/ico/add_black.svg" alt="next" width={100} height={100}/>
-      </Button>
+          );
+        })}
       </div>
+      {dataSet.length > limit && (
+        <Button className="btn-next" onClick={handleNextButton}>
+          <Image
+            src="/ico/chevron_right_black.svg"
+            alt="next"
+            className="imgLight"
+            width={30}
+            height={30}
+          />
+          <Image
+            src="/ico/chevron_right_white.svg"
+            alt="next"
+            className="imgDark"
+            width={30}
+            height={30}
+          />
+        </Button>
+      )}
     </div>
   );
 }
